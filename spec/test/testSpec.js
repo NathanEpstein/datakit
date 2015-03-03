fb5332033eb4f0d6acd91a481226e4cf057b8abe
NathanEpstein <ne2210@columbia.edu>
2015-03-03 11:29:50 -0500

    added uniform random array

--- spec/test/testSpec.js
+++ spec/test/testSpec.js
@@ -2,6 +2,6 @@ var datakit = require('../../lib/datakit.js');
 
 describe('a function',function(){
   it('should do something',function(){
-    expect('condition').toBe(true);
+    expect(true).toBe(true);
   });
 })
