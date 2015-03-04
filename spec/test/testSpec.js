911f3673ea900bc959d04307b1e070aecd42f007
NathanEpstein <ne2210@columbia.edu>
2015-03-04 14:50:55 -0500

    added some testing

--- spec/test/testSpec.js
+++ spec/test/testSpec.js
@@ -1,7 +1,29 @@
-var datakit = require('../../lib/datakit.js');
+var dk = require('../../lib/datakit.js');
 
-describe('a function',function(){
-  it('should do something',function(){
-    expect(true).toBe(true);
+describe('isclose',function(){
+  it('should return true for close numbers',function(){
+    expect(dk.isclose(0,Math.pow(10,-15))).toBe(true);
   });
-})
+  it('should return false for distant numbers',function(){
+    expect(dk.isclose(0,Math.pow(10,-5))).toBe(false);
+  });
+});
+
+describe('sum',function(){
+  var top = 10000;
+  var nums = [];
+  for (var i=0; i<=top; i++){
+    nums.push(i);
+  }
+  it('should accurately sum numbers',function(){
+    expect(dk.sum(nums)).toBe((top*(top+1)/2));
+  });
+
+  var floats = [0.1,0.2];
+  it('should minimize errors in floating point arithmetic',function(){
+    expect(dk.isclose(0.3,dk.sum(floats))).toBe(true);
+  });
+});
+
+
+
