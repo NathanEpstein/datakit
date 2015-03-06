08511bd2762b74a3cfc0225bdc1c31be65217ce8
NathanEpstein <ne2210@columbia.edu>
2015-03-05 21:25:07 -0500

    added tests

--- spec/test/testSpec.js
+++ spec/test/testSpec.js
@@ -9,6 +9,13 @@ describe('isclose',function(){
   });
 });
 
+var floats = [];
+for (var i=0; i<10000; i++){
+  floats.push(0.1);
+  floats.push(0.2);
+  floats.push(0.3);
+}
+
 describe('sum',function(){
   var top = 10000;
   var nums = [];
@@ -19,11 +26,41 @@ describe('sum',function(){
     expect(dk.sum(nums)).toBe((top*(top+1)/2));
   });
 
-  var floats = [0.1,0.2];
   it('should minimize errors in floating point arithmetic',function(){
-    expect(dk.isclose(0.3,dk.sum(floats))).toBe(true);
+    expect(dk.isclose(6000,dk.sum(floats))).toBe(true);
   });
 });
+
+describe('mean',function(){
+  var nums = [-10,15,25,0,-5];
+  it('should accurately compute the mean of numbers',function(){
+    expect(dk.mean(nums)).toBe(5);
+  });
+
+  it('should minimize errors in floating point arithmetic',function(){
+    expect(dk.isclose(0.2,dk.mean(floats))).toBe(true);
+  });
+});
+
+//FIX THIS TEST SPEC
+describe('prod',function(){
+  var top = 10;
+  var nums = [];
+  for (var i=1; i<=top; i++){
+    nums.push(i);
+  }
+  it('should accurately compute a product of numbers',function(){
+    expect(dk.prod(nums)).toBe(3628800);
+  });
+
+  var floats = [0.1,0.2,0.3,0.4];
+  it('should minimize errors in floating point arithmetic',function(){
+    expect(dk.isclose(dk.prod(floats),0.0024)).toBe(true);
+  });
+});
+
+
+
 
 
 
