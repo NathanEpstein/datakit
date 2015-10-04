84a2f6705f899993f7d6073806da3bae584a46be
NathanEpstein <ne2210@columbia.edu>
2015-10-04 12:24:21 -0400

    some more linting

--- README.md
+++ README.md
@@ -21,36 +21,36 @@ var dk = require('datakitjs');
 //READ A CSV FILE
 
 //file.csv
-// COL1,COL2
-// val11,val12
-// val21,val22
+// COL1, COL2
+// val11, val12
+// val21, val22
 
-dk.csv('file.csv',function(data){
+dk.csv('file.csv', function(data) {
   console.log(data);
 });
 
 //Output:
-//[{COL1:val11,COL2:val12},{COL1:val21,COL2:val22}]
+//[{ COL1: val11, COL2: val12 }, { COL1: val21, COL2: val22 }]
 
 
 //GET A COLUMN FROM AN ARRAY OF ROW OBJECTS
-dk.csv('file.csv',function(data){
-  var c2 = dk.col(data,'COL2');
+dk.csv('file.csv', function(data) {
+  var c2 = dk.col(data, 'COL2');
   console.log(c2);
 });
 
 //Output:
-//[val12,val22]
+//[val12, val22]
 
 // By default, dk.csv will convert all values to strings. You can convert select
-// columns to numbers by passing an array of column names to 'dk.numeric'. 
+// columns to numbers by passing an array of column names to 'dk.numeric'.
 
 //file2.csv
 // COL1, COL2
 // val11, 1
 // val21, 2
 
-dk.csv('file2.csv', function(data){
+dk.csv('file2.csv', function(data) {
   var d = dk.numeric(data, ['COL2'], 0) // The third parameter value will be filled
   // in to blank cells. Its default value is 0.
   var c2 = dk.col(d, 'COL2');
@@ -58,7 +58,7 @@ dk.csv('file2.csv', function(data){
 });
 
 //Output:
-//[1,2]
+//[1, 2]
 
 
 //PLOT ARRAY(S) OF DATA
@@ -68,10 +68,10 @@ var plot = dk.plot(x1); // x1 will be plotted on the y-axis, index on the x-axis
 //dk.plot returns a string of the html used to create the plot.
 
 
-var labels = dk.seq(1,25);
+var labels = dk.seq(1, 25);
 var x2 = dk.norm(25);
 var x3 = dk.norm(25);
-dk.plot(labels,x1,x2,x3); //labels on the x-axis, the next three arrays will each be plotted on the y-axis
+dk.plot(labels, x1, x2, x3); //labels on the x-axis, the next three arrays will each be plotted on the y-axis
 ```
 
 ###Statistical Methods
@@ -80,29 +80,29 @@ dk.plot(labels,x1,x2,x3); //labels on the x-axis, the next three arrays will eac
 var dk = require('datakitjs');
 
 //MEAN OF AN ARRAY
-dk.mean([1,2,3]); //returns 2
+dk.mean([1, 2, 3]); //returns 2
 
 //STANDARD DEVIATION AND VARIANCE OF AN ARRAY
-dk.sd([1,2,3]); //returns 1
-dk.vari([1,2,3]); //returns 1
+dk.sd([1, 2, 3]); //returns 1
+dk.vari([1, 2, 3]); //returns 1
 
 //COVARIANCE OF TWO ARRAYS
-dk.cov([1,2,3],[3,2,1]); //returns -1
+dk.cov([1, 2, 3], [3, 2, 1]); //returns -1
 
 //SIMPLE LINEAR REGRESSION
 
-var x = [1,2,3];
-var y = [2,1,3];
+var x = [1, 2, 3];
+var y = [2, 1, 3];
 
-var model = dk.reg(x,y);
+var model = dk.reg(x, y);
 
 // model.f is a function that returns the estimated y for an input x (estimated via standard OLS regression)
-// model.f = function(x){
+// model.f = function(x) {
 //  return (a + b * x);
-// }
+// };
 
 // model.pts is an array of the estimated y for each element of x
-// model.pts = [1.5,2,2.5];
+// model.pts = [1.5, 2, 2.5];
 
 ```
 
@@ -112,31 +112,31 @@ var dk = require('datakitjs');
 
 //GENERATE AN ARRAY WITH A SEQUENCE OF NUMBERS
 
-dk.seq(1,5); //returns [1,2,3,4,5]
+dk.seq(1, 5); //returns [1, 2, 3, 4, 5]
 
-dk.seq(0,1,0.25); //returns [0,0.25,0.5,0.75,1]
+dk.seq(0, 1, 0.25); //returns [0, 0.25, 0.5, 0.75, 1]
 
 //GENERATE AN ARRAY WITH REPEATED VALUE
 
-dk.rep(1,5); //returns [1,1,1,1,1]
+dk.rep(1, 5); //returns [1, 1, 1, 1, 1]
 
 //CHECK IF NUMBERS ARE CLOSE
-dk.isclose(0,Math.pow(10,-15)); //returns true
+dk.isclose(0, Math.pow(10, -15)); //returns true
 
-dk.isclose(0,Math.pow(10,-5)); //returns false
+dk.isclose(0, Math.pow(10, -5)); //returns false
 
 //SUM AN ARRAY OF NUMBERS
 //uses Kahan summation
 
-dk.sum([1,2,3]); //returns 6
+dk.sum([1, 2, 3]); //returns 6
 
 //PRODUCT OF AN ARRAY OF NUMBERS
 //implementation from 'Accurate Floating Point Product' - Stef Graillat
 
-dk.prod([1,2,3]); //returns 6
+dk.prod([1, 2, 3]); //returns 6
 
 //MAX AND MIN OF AN ARRAY
-var x = [1,2,3];
+var x = [1, 2, 3];
 dk.min(x); //returns 1
 dk.max(x); //returns 3
 
@@ -148,16 +148,16 @@ var dk = require('datakitjs');
 
 //GET AN ARRAY OF EXPONENTIALLY DISTRIBUTED VALUES
 
-dk.exp(3,1); //returns [ 0.3584189321510761, 1.0466439500242446, 0.08887770301056963 ]
+dk.exp(3, 1); //returns [0.3584189321510761, 1.0466439500242446, 0.08887770301056963]
 
 
 //GET AN ARRAY OF NORMALLY DISTRIBUTED VALUES
 
-dk.norm(3,0,1); //returns [ -1.709768103193772, 0.23530041388459744, 0.4431320382580479 ]
+dk.norm(3, 0, 1); //returns [-1.709768103193772, 0.23530041388459744, 0.4431320382580479]
 
 //GET AN ARRAY OF UNIFORMLY DISTRIBUTED VALUES
 
-dk.uni(3); //returns [ 0.30658303829841316, 0.1601463456172496, 0.8538850131444633 ]
+dk.uni(3); //returns [0.30658303829841316, 0.1601463456172496, 0.8538850131444633]
 
 ```
 
--- spec/test/testSpec.js
+++ spec/test/testSpec.js
@@ -3,122 +3,122 @@ var dk = require('../../lib/datakit.js');
 var fs = require('fs');
 var exec = require('child_process').exec;
 
-describe('isclose',function(){
-  it('should return true for close numbers',function(){
-    expect(dk.isclose(0,Math.pow(10,-15))).toBe(true);
+describe('isclose', function() {
+  it('should return true for close numbers', function() {
+    expect(dk.isclose(0, Math.pow(10, -15))).toBe(true);
   });
-  it('should return false for distant numbers',function(){
-    expect(dk.isclose(0,Math.pow(10,-5))).toBe(false);
+  it('should return false for distant numbers', function() {
+    expect(dk.isclose(0,Math.pow(10, -5))).toBe(false);
   });
 });
 
 var floats = [];
-for (var i=0; i<10000; i++){
+for (var i = 0; i < 10000; i++) {
   floats.push(0.1);
   floats.push(0.2);
   floats.push(0.3);
 }
 
-describe('sum',function(){
+describe('sum', function() {
   var top = 10000;
   var nums = [];
-  for (var i=0; i<=top; i++){
+  for (var i = 0; i <= top; i++) {
     nums.push(i);
   }
-  it('should accurately sum numbers',function(){
-    expect(dk.sum(nums)).toBe((top*(top+1)/2));
+  it('should accurately sum numbers', function() {
+    expect(dk.sum(nums)).toBe((top * (top + 1) / 2));
   });
 
-  it('should minimize errors in floating point arithmetic',function(){
-    expect(dk.isclose(6000,dk.sum(floats))).toBe(true);
+  it('should minimize errors in floating point arithmetic', function() {
+    expect(dk.isclose(6000, dk.sum(floats))).toBe(true);
   });
 });
 
-describe('mean',function(){
-  var nums = [-10,15,25,0,-5];
-  it('should accurately compute the mean of numbers',function(){
+describe('mean',function() {
+  var nums = [-10, 15, 25, 0, -5];
+  it('should accurately compute the mean of numbers', function() {
     expect(dk.mean(nums)).toBe(5);
   });
 
-  it('should minimize errors in floating point arithmetic',function(){
-    expect(dk.isclose(0.2,dk.mean(floats))).toBe(true);
+  it('should minimize errors in floating point arithmetic', function() {
+    expect(dk.isclose(0.2, dk.mean(floats))).toBe(true);
   });
 });
 
-describe('prod',function(){
+describe('prod', function() {
   var top = 10;
   var nums = [];
-  for (var i=1; i<=top; i++){
+  for (var i = 1; i <= top; i++) {
     nums.push(i);
   }
-  it('should accurately compute a product of numbers',function(){
+  it('should accurately compute a product of numbers', function() {
     expect(dk.prod(nums)).toBe(3628800);
   });
 
-  var floats_ = [Math.pow(10,75)].concat(floats.slice(0,99));
-  it('should minimize errors in floating point arithmetic',function(){
-    expect(dk.isclose(dk.prod(floats_),47.751966659678405306351616)).toBe(true);
+  var floats_ = [Math.pow(10, 75)].concat(floats.slice(0, 99));
+  it('should minimize errors in floating point arithmetic', function() {
+    expect(dk.isclose(dk.prod(floats_), 47.751966659678405306351616)).toBe(true);
   });
 });
 
-describe('max and min',function(){
-  var nums = [10,-2,23,12,43,123213,2];
-  it('should accurately identify the maximum',function(){
+describe('max and min',function() {
+  var nums = [10, -2, 23, 12, 43, 123213, 2];
+  it('should accurately identify the maximum', function() {
     expect(dk.max(nums)).toBe(123213);
   });
 
-  it('should accurately identify the minimum',function(){
+  it('should accurately identify the minimum', function() {
     expect(dk.min(nums)).toBe(-2);
   });
 });
 
-describe('cov, vari, and sd',function(){
-  var nums = [-10,-5,0,5,10];
-  var nums_ = [0,10,20,30,40];
-  it('should accurately compute covariance', function(){
+describe('cov, vari, and sd', function() {
+  var nums = [-10, -5, 0, 5, 10];
+  var nums_ = [0, 10, 20, 30, 40];
+  it('should accurately compute covariance', function() {
     expect(dk.cov(nums,nums_)).toBe(125);
   });
-  it('should accurately compute variance', function(){
+  it('should accurately compute variance', function() {
     expect(dk.vari(nums)).toBe(62.5);
   });
-  it('should accurately compute standard deviation', function(){
+  it('should accurately compute standard deviation', function() {
     expect(dk.sd(nums_)).toBe(Math.sqrt(250));
   });
 });
 
-describe('random number generators', function(){
+describe('random number generators', function() {
   var u = dk.uni(100);
   var n = dk.norm(100);
   var e = dk.exp(100);
   var n_ = dk.norm(3);
-  it('should have the right length',function(){
+  it('should have the right length', function() {
     expect(u.length).toBe(100);
     expect(n.length).toBe(100);
     expect(e.length).toBe(100);
     expect(n_.length).toBe(3);
   });
-  it('should generate normals from Box-Muller',function(){
+  it('should generate normals from Box-Muller', function() {
     var z1 = n[0];
     var z2 = n[1];
     var u = Math.exp(Math.pow(Math.pow(z1,2)+Math.pow(z2,2),2)*(-2));
     expect(u).toBeGreaterThan(0);
     expect(u).toBeLessThan(1);
   });
-  it('should have the correct uniform boundaries',function(){
+  it('should have the correct uniform boundaries', function() {
     expect(dk.min(u)).toBeGreaterThan(0);
     expect(dk.max(u)).toBeLessThan(1);
   });
-  it('should generate exponentials from uniforms',function(){
+  it('should generate exponentials from uniforms', function() {
     u = Math.exp(-e[0]);
     expect(u).toBeGreaterThan(0);
     expect(u).toBeLessThan(1);
   });
 });
 
-describe('csv and col', function(){
+describe('csv and col', function() {
   var val, d;
   beforeEach(function(done) {
-    dk.csv('spec/test/test.csv',function(data){
+    dk.csv('spec/test/test.csv', function(data) {
       d = data;
       val = d[0].COL1;
       done();
@@ -132,43 +132,43 @@ describe('csv and col', function(){
   });
 });
 
-describe('seq',function(){
-  it('should return accurate sequences',function(){
+describe('seq', function() {
+  it('should return accurate sequences', function() {
     expect(dk.seq(1,10).length).toBe(10);
     expect(dk.seq(-10,10,2)[5]).toBe(0);
   });
 });
 
-describe('reg',function(){
+describe('reg', function() {
   var x = [1,2,3,5];
   var y = [2,4,6,10];
   var m = dk.reg(x,y);
-  it('should return an accurate model',function(){
+  it('should return an accurate model', function() {
     expect(m.f(4)).toBe(8);
   });
-  it('should return an accurate interpolation of the linear model', function(){
+  it('should return an accurate interpolation of the linear model', function() {
     expect(m.pts[0]).toBe(2);
   });
 });
 
-describe('rep',function(){
+describe('rep', function() {
   var arr = dk.rep(0.5,100);
-  it('should have the correct length',function(){
+  it('should have the correct length', function() {
     expect(arr.length).toBe(100);
   });
-  it('should have the correct value',function(){
+  it('should have the correct value', function() {
     expect(arr[0]).toBe(0.5);
   });
 });
 
-describe('plot',function(){
+describe('plot', function() {
   var p;
   beforeEach(function(done) {
     p = dk.plot([1,2,3])
     done();
   });
 
-  it('should return an html string',function(done){
+  it('should return an html string', function(done) {
     expect(p.slice(0,6)).toBe('<html>');
     expect(p.slice(p.length-7,p.length)).toBe('</html>')
     done();
@@ -194,6 +194,4 @@ describe('numeric', function() {
     expect(dk.col(d, 'COL2')[2]).toBe(6253);
     done();
   });
-
-
 });
